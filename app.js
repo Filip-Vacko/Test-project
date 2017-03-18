a3ad55796236b4fa542ec2d0662231ffa9ff24a7
Filip Vacko <filip.vacko@gmail.com>
2017-03-18 15:09:26 +0100

    Add a simple express post request

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-var http = require("http");
 var testModule = require("./testModule");
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json()
+var receivedData = "";
+
 
 testModule();
 
@@ -10,4 +13,11 @@ app.listen(3000);
 app.get("/", function (req, res) {
     res.send("Hello World");
 
-})
+});
+
+app.post("/", jsonParser, function (req, res) {
+    receivedData = req.body.id;
+    res.send("Hello World");
+    console.log(receivedData);
+
+});
